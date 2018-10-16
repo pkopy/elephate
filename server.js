da5bd74efb30ee7b7cb898f3a7caf227d5f25e17
pkopy <pawel.kopycki@gmail.com>
2018-10-16 12:10:08 +0200

    adding name of object

--- server.js
+++ server.js
@@ -11,18 +11,7 @@ const url = require('url')
 
 const puppeteer = require('puppeteer');
 const data = {
-  dataJS:{
-    // enableJS:'false',
-    numb:'',
-    href:[],
-    imgSrc:''
-  },
-  
-  dataWithoutJS: {
-    numb:'',
-    href:[],
-    imgSrc:''
-  }
+
 
 }
 
@@ -31,51 +20,67 @@ const data = {
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-    (async () => {
-        const browser  = await puppeteer.launch()
-        //   {
-        //   ignoreHTTPSErrors: true,
-        //   args: ['--disable-setuid-sandbox', '--no-sandbox']
-        // })
-        let test = url.parse(req.url, true).query
-        
-        const page = await browser.newPage();
-        // test.mama === 'true'?await page.setJavaScriptEnabled(true):await page.setJavaScriptEnabled(false)
-        
-        // await page.setRequestInterceptionEnabled(true)
-        
-        // page.on('request', intercepted => intercepted.continue())
-        await page.setJavaScriptEnabled(true)
-        await page.goto('http://youtube.com', {waitUntil: 'networkidle0'})
-        await page.$$eval('a', divs => divs.length).then((x)=> data.dataJS.numb = x);
-        await page.$$eval('a', el => el.map(n => n.href)).then(x => data.dataJS.href = x)
-        await page.screenshot({path: 'public/example_with_JS.png'});
-        data.dataJS.imgSrc = 'http://localhost:3000/example_with_JS.png'
+  (async () => {
+    const browser = await puppeteer.launch()
+    //   {
+    //   ignoreHTTPSErrors: true,
+    //   args: ['--disable-setuid-sandbox', '--no-sandbox']
+    // })
+    let test = url.parse(req.url, true).query
 
-        await page.setJavaScriptEnabled(false)
-        await page.goto('http://youtube.com', {waitUntil: 'networkidle0'})
-        await page.$$eval('a', divs => divs.length).then((x)=> data.dataWithoutJS.numb = x);
-        await page.$$eval('a', el => el.map(n => n.href)).then(x => data.dataWithoutJS.href = x)
-        await page.screenshot({path: 'public/example_without_JS.png'});
-        data.dataWithoutJS.imgSrc = 'http://localhost:3000/example_without_JS.png'
+    const page = await browser.newPage();
+    // test.mama === 'true'?await page.setJavaScriptEnabled(true):await page.setJavaScriptEnabled(false)
 
-        // console.log(divs)
-        
-        // console.log(divs)
-        console.log('page load DONE')
-        // if(test.mama === 'true') {
-        //   await page.screenshot({path: 'public/example_with_JS.png'});
-        //   data.imgSrc = 'http://localhost:3000/example_with_JS.png'
-        // }else {
-        //   await page.screenshot({path: 'public/example_without_JS.png'});
-        //   data.imgSrc = 'http://localhost:3000/example_without_JS.png'
-        // }
-        await browser.close();
-        
-      })().then(() => JSON.stringify(data)).then(data => {
-          
-        res.send(data)});
-    
+    // await page.setRequestInterceptionEnabled(true)
+
+    const reg = /\./g
+    reg.test(test.site)
+    let name = test.site.slice(7, reg.lastIndex - 1)
+
+
+    // page.on('request', intercepted => intercepted.continue())
+    await page.setJavaScriptEnabled(true)
+    data[name] = {}
+    await page.goto(test.site, {
+      waitUntil: 'networkidle0'
+    })
+    await page.$$eval('a', divs => divs.length).then((x) => data[name].numb = x);
+    await page.$$eval('a', el => el.map(n => n.href)).then(x => data[name].href = x)
+    await page.screenshot({
+      path: `public/${name}_with_JS.png`
+    });
+    data[name].imgSrc = `http://localhost:3000/${name}_with_JS.png`
+
+    await page.setJavaScriptEnabled(false)
+    await page.goto(test.site, {
+      waitUntil: 'networkidle0'
+    })
+    await page.$$eval('a', divs => divs.length).then((x) => data[name].numbWithoutJS = x);
+    await page.$$eval('a', el => el.map(n => n.href)).then(x => data[name].hrefWithoutJS = x)
+    await page.screenshot({
+      path: `public/${name}_without_JS.png`
+    });
+    data[name].imgSrcWithoutJS = `http://localhost:3000/${name}_without_JS.png`
+
+
+    // console.log(divs)
+
+    // console.log(divs)
+    console.log('page load DONE')
+    // if(test.mama === 'true') {
+    //   await page.screenshot({path: 'public/example_with_JS.png'});
+    //   data.imgSrc = 'http://localhost:3000/example_with_JS.png'
+    // }else {
+    //   await page.screenshot({path: 'public/example_without_JS.png'});
+    //   data.imgSrc = 'http://localhost:3000/example_without_JS.png'
+    // }
+    await browser.close();
+
+  })().then(() => JSON.stringify(data)).then(data => {
+
+    res.send(data)
+  });
+
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
