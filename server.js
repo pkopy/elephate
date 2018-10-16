22b06b32d62e45b486bf1ddcd3a9fe77623ba431
pkopy <pawel.kopycki@gmail.com>
2018-10-16 13:28:54 +0200

    changing timeout

--- server.js
+++ server.js
@@ -10,60 +10,60 @@ const url = require('url')
 // const x = test.test()
 
 const puppeteer = require('puppeteer');
-const sites = ["http://angular.io", "http://youtube.com", "http://onet.pl", "http://nytimes.com", "http://wp.pl"]
+const sites = ['http://wpengine.com', "http://angular.io", "http://youtube.com", "http://onet.pl", "http://nytimes.com", "http://wp.pl"]
 const data = {}
-
-
+// const sites = ['http://wpengine.com']
+// 
 
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-  
-  (async () => {
-    for(let site of sites){
-    const browser = await puppeteer.launch()
-    //   {
-    //   ignoreHTTPSErrors: true,
-    //   args: ['--disable-setuid-sandbox', '--no-sandbox']
-    // })
-    let test = url.parse(req.url, true).query
 
-    
-    
+  (async () => {
+    for (let site of sites) {
+      const browser = await puppeteer.launch()
+      //   {
+      //   ignoreHTTPSErrors: true,
+      //   args: ['--disable-setuid-sandbox', '--no-sandbox']
+      // })
+      let test = url.parse(req.url, true).query
+
+
+
       const page = await browser.newPage();
       const reg = /\./g
-    reg.test(site)
-    let name = site.slice(7, reg.lastIndex - 1)
+      reg.test(site)
+      let name = site.slice(7, reg.lastIndex - 1)
 
 
-    await page.setJavaScriptEnabled(true)
-    data[name] = {}
-    await page.goto(site,  {timeout:50000,
-      waitUntil: 'networkidle0'
-    })
-    await page.$$eval('a', divs => divs.length).then((x) => data[name].numb = x);
-    await page.$$eval('a', el => el.map(n => n.href)).then(x => data[name].href = x)
-    await page.screenshot({
-      path: `public/${name}_with_JS.png`
-    });
-    data[name].imgSrc = `http://localhost:3000/${name}_with_JS.png`
+      await page.setJavaScriptEnabled(true)
+      data[name] = {}
+      await page.goto(site, {
+        timeout: 50000
+      }).catch(err => console.log(err))
+      await page.$$eval('a', divs => divs.length).then((x) => data[name].numb = x);
+      await page.$$eval('a', el => el.map(n => n.href)).then(x => data[name].href = x)
+      await page.screenshot({
+        path: `public/${name}_with_JS.png`
+      });
+      data[name].imgSrc = `http://localhost:3000/${name}_with_JS.png`
 
-    await page.setJavaScriptEnabled(false)
-    await page.goto(site, {
-      waitUntil: 'networkidle0'
-    })
-    await page.$$eval('a', divs => divs.length).then((x) => data[name].numbWithoutJS = x);
-    await page.$$eval('a', el => el.map(n => n.href)).then(x => data[name].hrefWithoutJS = x)
-    await page.screenshot({
-      path: `public/${name}_without_JS.png`
-    });
-    data[name].imgSrcWithoutJS = `http://localhost:3000/${name}_without_JS.png`
+      await page.setJavaScriptEnabled(false)
+      await page.goto(site, {
+        timeout: 50000
+      })
+      await page.$$eval('a', divs => divs.length).then((x) => data[name].numbWithoutJS = x);
+      await page.$$eval('a', el => el.map(n => n.href)).then(x => data[name].hrefWithoutJS = x)
+      await page.screenshot({
+        path: `public/${name}_without_JS.png`
+      });
+      data[name].imgSrcWithoutJS = `http://localhost:3000/${name}_without_JS.png`
 
-    await browser.close();
-    
-  }
-  // console.log(data)
+      await browser.close();
+
+    }
+    // console.log(data)
 
   })()
   .then(() => JSON.stringify(data)).then(data => {
@@ -71,7 +71,7 @@ app.get('/', (req, res) => {
     res.send(data)
   })
   // .catch(err => console.log(err));
-  
+
 })
 
 app.get('/test', (req, res) => {
