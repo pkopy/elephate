0afa905e35e6aa3d416daad817358e5ee72666a6
pkopy <pawel.kopycki@gmail.com>
2018-10-17 15:40:00 +0200

    adding favicon

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -6,7 +6,8 @@ class Header extends Component {
   render() {
     return (
       <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />    
+          <img src={logo} className="App-logo" alt="logo" />   
+          <h1 className="header-text">PUPEETER AND REACT </h1> 
       </header>
     )
   }
