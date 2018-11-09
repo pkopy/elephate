aef933e313db7a8cb17744360495c6a5c74bbdaa
pkopy <pawel.kopycki@gmail.com>
2018-11-09 12:54:49 +0100

    adding token

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,31 +10,49 @@ class App extends Component {
     isLoad: true,
     error:"We have error",
     details:'',
-    progress: 0
+    progress: 0,
+    token: ''
   }
-
-  getData = () => {
-    this.setState({data:[]})
-    this.setState({isLoad: false})
-    this.setState({progress:0})
-    fetch('http://localhost:3000/').then(res => res.json())
-    .then(dataRes => {
-      this.setState({data:dataRes});
-      this.setState({isLoad:true})
-      
-    }).catch()
+  componentDidMount = () =>{
+    let token = Math.random().toString(36).substr(-8)
+    this.setState({token})
+    console.log(this.state.token)
+  }
+  setToken = () => {
     
-    let inter = setInterval(() => {
+  }
+  getData = () => {
+    this.setToken()
+    if(this.state.isLoad) {
+      
+      this.setState({data:[]})
+      this.setState({isLoad: false})
+      this.setState({progress:0})
+      fetch('http://localhost:3000/', {
+        headers :{token : this.state.token}
+      }).then(res => res.json())
+      .then(dataRes => {
+        this.setState({data:dataRes});
+        this.setState({isLoad:true})
+        
+      }).catch()
+      
+      let inter = setInterval(() => {
+        console.log(this.state.token)
+        fetch('http://localhost:3000/count',{
+          
+          headers :{token: this.state.token}
+        }).then(res => res.json())
+        .then(
+          data => {
+            this.setState({progress:data})
+            if(data > 5) {
+              clearInterval(inter)
+            }
+          })       
+      }, 3000)
 
-      fetch('http://localhost:3000/count').then(res => res.json())
-      .then(
-        data => {
-          this.setState({progress:data})
-          if(data > 5) {
-            clearInterval(inter)
-          }
-        })       
-    }, 3000)
+    }
 
       
 
--- puppeteer.js
+++ puppeteer.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = 3000
+const path = require('path')
 
 
 const puppeteer = require('puppeteer');
@@ -14,18 +15,24 @@ const sites = [
   "http://wp.pl"
 ]
 let data = [];
-let len = {a: data.length};
- let count =0;
 
+let count =0;
+let db = {};
+let progress;
+let baseDir = path.join(__dirname,'/public/')
+console.log(baseDir)
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-  data = [];
-
+  
+  let data1 = [];
+  let token = req.headers.token;
+  
   (async () => {
-    
+    db[token] = []
     for (let site of sites) {
+      
       const browser = await puppeteer.launch()
       const page = await browser.newPage();
       
@@ -34,6 +41,7 @@ app.get('/', (req, res) => {
       let name = site.slice(7, reg.lastIndex - 1)
 
       await page.setJavaScriptEnabled(true)
+      
       obj = {}
       obj.name = name;
       obj.site = site;
@@ -43,7 +51,7 @@ app.get('/', (req, res) => {
       await page.$$eval('a', divs => divs.length).then(x => obj.numb = x);
       await page.$$eval('a', el => el.map(n => n.href)).then(x => obj.href = x)
       await page.screenshot({
-        path: `public/${name}${count}_with_JS.png`
+        path: `${baseDir}${name}${count}_with_JS.png`
       });
       obj.imgSrc = `http://localhost:3000/${name}${count}_with_JS.png`
 
@@ -54,23 +62,35 @@ app.get('/', (req, res) => {
       await page.$$eval('a', divs => divs.length).then(x => obj.numbWithoutJS = x);
       await page.$$eval('a', el => el.map(n => n.href)).then(x => obj.hrefWithoutJS = x)
       await page.screenshot({
-        path: `public/${name}${count}_without_JS.png`
+        path: `${baseDir}${name}${count}_without_JS.png`
       });
       obj.imgSrcWithoutJS = `http://localhost:3000/${name}${count}_without_JS.png`
-      data.push(obj)
+      data1.push(obj)
+      db[token] = data1
+      
       await browser.close();
 
+      console.log(token)
     }
+    // console.log(db)
     count++
+    return token
   })()
-  .then(() => JSON.stringify(data)).then(data => {
+  .then((token) => JSON.stringify(db[token])).then(data => {
     res.send(data)
   })
   
 })
 
 app.get('/count', (req, res) => {
-   res.send(JSON.stringify(data.length))
+  let token1 = req.headers.token;
+  console.log(token1)
+  if(db[token1]){
+    res.send(JSON.stringify(db[token1].length))
+
+  }else {
+    res.send(JSON.stringify(0))
+  }
 }
 )
 
